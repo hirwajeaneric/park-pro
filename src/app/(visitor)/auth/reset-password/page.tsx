719400c909889e2b659ea2e12dc23c44789cbddc
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-04-04 00:21:09 +0200

    update: Adding suspense
    Added suspense to files that have forms with useSearchParams hook.

--- src/app/(visitor)/auth/reset-password/page.tsx
+++ src/app/(visitor)/auth/reset-password/page.tsx
@@ -1,16 +1,19 @@
 import ResetPasswordForm from "@/components/forms/ResetPasswordForm";
 import { PageBanner } from "@/components/widget/PageBanner";
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <>
-      <PageBanner 
-        title="Change Password?" 
-        backgroundImage={"/Gabon_Loango_National_Park_Southern_Camping_Ground_bar_with_a_view.jpeg"} 
+      <PageBanner
+        title="Change Password?"
+        backgroundImage={"/Gabon_Loango_National_Park_Southern_Camping_Ground_bar_with_a_view.jpeg"}
       />
       <section className="py-8 bg-white mx-auto w-full flex flex-col items-center justify-center">
         <div className="container px-4 flex w-full items-center justify-center">
-          <ResetPasswordForm />
+          <Suspense fallback={<div>Loading...</div>}>
+            <ResetPasswordForm />
+          </Suspense>
         </div>
       </section>
     </>
--- src/app/(visitor)/auth/signin/page.tsx
+++ src/app/(visitor)/auth/signin/page.tsx
@@ -1,5 +1,6 @@
 import SignInForm from "@/components/forms/SignInForm";
 import { PageBanner } from "@/components/widget/PageBanner";
+import { Suspense } from "react";
 
 export default function page() {
   return (
@@ -7,7 +8,9 @@ export default function page() {
       <PageBanner title="Sign In to Your Account" backgroundImage={"/Gabon_Loango_National_Park_Southern_Camping_Ground_bar_with_a_view.jpeg"} />
       <section className="py-8 bg-white mx-auto w-full flex flex-col items-center justify-center">
         <div className="container px-4 flex w-full items-center justify-center">
-          <SignInForm />
+          <Suspense fallback={<div>Loading...</div>}>
+            <SignInForm />
+          </Suspense>
         </div>
       </section>
     </>
